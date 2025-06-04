e5c4bb12dc4d090cab1353a4b0ec4437e90b9d58
jhs512 <jangka512@gmail.com>
2025-06-04 10:51:05 +0900

    no args

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -3,6 +3,7 @@ package com.back;
 public class Main {
     public static void main(String[] args) {
         WiseSaying wiseSaying1 = new WiseSaying(1, "나의 죽음을 적들에게 알리지 말라.", "이순신");
+        WiseSaying wiseSaying2 = new WiseSaying();
 
         System.out.println(wiseSaying1);
     }
--- src/main/java/com/back/WiseSaying.java
+++ src/main/java/com/back/WiseSaying.java
@@ -1,11 +1,9 @@
 package com.back;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
