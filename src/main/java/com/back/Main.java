b340a7ac17a5df941e19d224c1176090a965d4a8
jhs512 <jangka512@gmail.com>
2025-06-04 10:43:42 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -2,6 +2,8 @@ package com.back;
 
 public class Main {
     public static void main(String[] args) {
+        WiseSaying wiseSaying1 = new WiseSaying(1, "나의 죽음을 적들에게 알리지 말라.", "이순신");
 
+        System.out.println(wiseSaying1);
     }
 }
