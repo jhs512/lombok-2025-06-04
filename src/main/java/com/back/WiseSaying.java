17c95149b44a608a6daf1f74ec87686a86a9d7a8
jhs512 <jangka512@gmail.com>
2025-06-04 10:49:30 +0900

    class level getter/setter

--- src/main/java/com/back/WiseSaying.java
+++ src/main/java/com/back/WiseSaying.java
@@ -5,28 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class WiseSaying extends Object {
-    @Getter
-    @Setter
     private int id;
     private String content;
     private String author;
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 
     @Override
     public String toString() {
