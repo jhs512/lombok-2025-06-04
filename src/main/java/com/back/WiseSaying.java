5fb1e832e4d12d050a7d0d8e0865cac89e61a12b
jhs512 <jangka512@gmail.com>
2025-06-04 10:49:55 +0900

    to string

--- src/main/java/com/back/WiseSaying.java
+++ src/main/java/com/back/WiseSaying.java
@@ -3,21 +3,14 @@ package com.back;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class WiseSaying extends Object {
     private int id;
     private String content;
     private String author;
-
-    @Override
-    public String toString() {
-        return "WiseSaying{" +
-                "id=" + id +
-                ", content='" + content + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
 }
